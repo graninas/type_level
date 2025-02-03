09fc6a54291dce912b09b9fc87e3ae70f9831437
Alexander Granin <graninas@gmail.com>
2025-02-03 18:00:48 +0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::marker::PhantomData;
+
+// Meta-interface for type-level interfaces.
+pub trait IInterface<I> {
+  type Interface;
+  // Could be defaulted to I but the feature is not supported:
+  // type Interface = I;
+}
+
+// Existential type-level wrapper for user-defined implementation types.
+pub struct Wrapper<I, T> (PhantomData::<(I, T)>);
+
+// Instance that connects together a kind and an implementation type
+// thus creating new type-level interface
+impl<I, T> IInterface<I> for Wrapper<I, T> {
+  type Interface = I;
+}
+
+// Universal evaluation mechanism
+pub trait Eval<Verb, Res>{
+  fn eval() -> Res;
+}
+
+// Universal evaluation mechanism (with context)
+pub trait EvalCtx<Ctx, Verb, Res>{
+  fn eval_ctx(ctx: Ctx) -> Res;
+}
